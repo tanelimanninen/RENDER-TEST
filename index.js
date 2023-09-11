6b0590148a8697a5a97272cf0d847d5faf6693cb
AA3977 <aa3977@student.jamk.fi>
2023-09-11 15:06:18 +0300

    Add frontend to project

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const unknownEndpoint = (request, response) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('dist'))
+app.use(express.static('build'))
 
 let notes = [
   {
