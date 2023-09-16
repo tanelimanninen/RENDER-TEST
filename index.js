721f464ca41ce289d67ab6c5d42e0b9fc4b1b147
AA3977 <aa3977@student.jamk.fi>
2023-09-16 11:26:12 +0300

    Update index.js and more requests

--- index.js
+++ index.js
@@ -19,10 +19,22 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+//MIDDLEWARE 3: INFORM USER WHEN ID DOESN'T EXIST
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
 
 
 //ROUTE 1: GET SERVER ROOT
@@ -38,26 +50,29 @@ app.get('/api/notes', (request, response) => {
 })
 
 //ROUTE 3: GET SINGLE JSON DATA OBJECT
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    //CONDITION 1: IF NOTE EXISTS
+    if (note) {
+      response.json(note)
+    } 
+    //CONDITION 2: IF NOTE DOESN'T EXIST
+    else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 //ROUTE 4: DELETE SINGLE JSON DATA OBJECT
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
-
-/*const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}*/
 
 //ROUTE 5: ADD NEW DATA
 app.post('/api/notes', (request, response) => {
@@ -80,8 +95,25 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+//ROUTE 6: UPDATE EXISTING DATA
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  .then(updatedNote => {
+    response.json(updatedNote)
+  })
+  .catch(error => next(error))
+})
+
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
