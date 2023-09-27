f8c866ef5f3e2a3ee2d15fbc26e17fdab695168d
AA3977 <aa3977@student.jamk.fi>
2023-09-27 14:00:07 +0300

    Update project with user configuration

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,13 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 //ROUTE 1: GET ALL DATA
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+
   response.json(notes)
 })
 
@@ -20,17 +24,23 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
-//ROUTE 3: ADD NEW DATA
+//ROUTE 3: MAKE NEW NOTE OBJECT
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   try {
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.status(201).json(savedNote)
   } catch(exception) {
     next(exception)
